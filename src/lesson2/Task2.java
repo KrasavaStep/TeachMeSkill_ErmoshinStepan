f8efb92d617a41ae2ebbb6684cbfad35d1948db0
esast <stepan.ermoshin@mail.ru>
2021-12-07 23:00:41 +0300

    lesson 2

---
+++ src/lesson2/Task2.java
@@ -0,0 +1,24 @@
+package lesson2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int first = 3;
+        short second = 7;
+        byte third = 5;
+        float fourth = 17f;
+        double fifth = 66;
+        float sixth;
+        int seventh;
+        double eighth;
+        int ninth;
+        long tenth;
+
+        sixth = second * fourth;
+        seventh = first / third;
+        eighth = fifth % second;
+        ninth = third - second;
+        tenth = first + second;
+
+        System.out.println(sixth + "\n" + seventh + "\n" + eighth + "\n" + ninth + "\n" + tenth );
+    }
+}
