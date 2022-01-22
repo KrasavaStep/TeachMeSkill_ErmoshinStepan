85a7c207c83822ddc708e6af8fa6d6448f0c73b5
esast <stepan.ermoshin@mail.ru>
2022-01-22 17:32:17 +0300

    new commit

---
+++ src/ReaderFromFile.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class ReaderFromFile {
+    static ArrayList<String> digitsListStr = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        readFromFile();
+        writeToFile(translateIntoDigits());
+    }
+
+    public static void readFromFile(){
+        File file = new File("src/digits.txt");
+        StringBuilder digit = new StringBuilder();
+
+        try (FileReader reader = new FileReader(file)){
+            int c;
+            while ((c = reader.read()) != -1){
+                if ((char)c != ' '){
+                    digit.append((char)c);
+                }
+                else {
+                    digitsListStr.add(digit.toString());
+                    digit.setLength(0);
+                }
+            }
+            digitsListStr.add(digit.toString());
+            digit.setLength(0);
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static ArrayList<Integer> translateIntoDigits(){
+        ArrayList<Integer> digitsListInt = new ArrayList<>();
+
+        for (String digit : digitsListStr) {
+            digitsListInt.add(Integer.parseInt(digit) * 10);
+        }
+
+        return digitsListInt;
+    }
+
+    public static void writeToFile(ArrayList<Integer> digitsList){
+
+        try (FileWriter writer = new FileWriter("src/new_digits.txt", false)){
+            for (int digit : digitsList) {
+                writer.write(digit + " ");
+            }
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
